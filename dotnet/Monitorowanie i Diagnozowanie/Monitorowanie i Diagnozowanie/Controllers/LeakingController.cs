a5b928ddd5fbb39c8a6b271346fa19d001e9d5a8
Madrianoliko <adrianmalik1001@gmail.com>
2024-06-04 09:53:17 +0200

    end of monitoring and diagnoze course

---
+++ dotnet/Monitorowanie i Diagnozowanie/Monitorowanie i Diagnozowanie/Controllers/LeakingController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Monitorowanie_i_Diagnozowanie.Services;
+
+namespace Monitorowanie_i_Diagnozowanie.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeakingController(MyCacheService cacheService) : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult AddDevice()
+        {
+            var device = new Device(Guid.NewGuid(), new string('x', 10 * 1024), "Kind");
+
+            cacheService.AddDevice(device);
+            return Ok();
+        }
+    }
+}
---
+++ dotnet/Monitorowanie i Diagnozowanie/Monitorowanie i Diagnozowanie/Services/MyCacheService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Concurrent;
+
+namespace Monitorowanie_i_Diagnozowanie.Services
+{
+    public record Device(Guid Id, string Name, string Kind);
+    public class MyCacheService
+    {
+        private readonly ConcurrentDictionary<Guid, Device> _devices = new();
+
+        public void AddDevice(Device device) =>
+            _devices.TryAdd(device.Id, device);
+    }
+}
